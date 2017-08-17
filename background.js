bcaf567c95f145c5a46c25f04ed1c8e6e7149b6e
mgottsacker34 <mgottsacker34@gmail.com>
2017-08-17 11:38:47 -0500

    Improved readability

--- background.js
+++ background.js
@@ -6,15 +6,19 @@ chrome.browserAction.onClicked.addListener(function() {
     //get current URL as string
     var currentUrl = tab[0].url;
     console.log("Current URL: " + currentUrl);
+
     //find last slash
     var pos = currentUrl.lastIndexOf("/");
     console.log("Last index of / is: " + pos);
-    //make new URL string that removes last slash to go up one directory
+
+    //check index to make sure it is the right /
     if(pos === currentUrl.length-1){
       console.log("Last index of / was last char of URL...going up one more step");
       pos = currentUrl.substring(0, pos).lastIndexOf("/");
       console.log("New last index of / is: " + pos);
     }
+
+    //make new URL string that removes last slash to go up one directory
     var newUrl = currentUrl.substring(0, pos);
     console.log("New URL: " + newUrl);
 
