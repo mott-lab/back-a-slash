96ec9aec7e638e12cc2fe96eadf817b62800b279
mgottsacker34 <mgottsacker34@gmail.com>
2017-08-17 11:28:11 -0500

    Navigates to new directory in new tab properly

--- background.js
+++ background.js
@@ -3,8 +3,24 @@ chrome.browserAction.onClicked.addListener(function() {
     currentWindow: true,
     active: true
   }, function(tab) {
+    //get current URL as string
+    var currentUrl = tab[0].url;
+    console.log("Current URL: " + currentUrl);
+    //find last slash
+    var pos = currentUrl.lastIndexOf("/");
+    console.log("Last index of / is: " + pos);
+    //make new URL string that removes last slash to go up one directory
+    if(pos === currentUrl.length-1){
+      console.log("Last index of / was last char of URL...going up one more step");
+      pos = currentUrl.substring(0, pos).lastIndexOf("/");
+      console.log("New last index of / is: " + pos);
+    }
+    var newUrl = currentUrl.substring(0, pos);
+    console.log("New URL: " + newUrl);
+
     chrome.tabs.create({
-      "url": "https://dev.opera.com"
+      console.log("Going to new URL...")
+      "url": newUrl
     });
   });
 });
